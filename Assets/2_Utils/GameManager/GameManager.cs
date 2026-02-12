56dac862189df438ca9e98b8f67ac3279b967f1a
TU-PJY <viska0319@icloud.com>
2026-02-12 10:18:37 +0900

    add GameManager

---
+++ Assets/2_Utils/GameManager/GameManager.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Inst;
+
+    void Awake()
+    {
+        if(Inst && Inst == this)
+        {
+            print("[GameManager] Instance is exist already. Destroy duplicated instance.");
+            DestroyImmediate(this);
+            return;
+        }
+
+        print("[GameManager] Created new instance.");
+        Inst = this;
+        DontDestroyOnLoad(this);
+    }
+}
